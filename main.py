8d16dcadd8fffd75f22b034a329b86eccdc5162f
AhmadSElsayed <sama_hamada@hotmail.com>
2017-12-10 13:08:33 +0200

    new

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Hello_World():
-    return 'Hello World'
-
-@app.route('/Test')
-def test():
-    return 'Test Check!'
+def hello_world():
+  return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
